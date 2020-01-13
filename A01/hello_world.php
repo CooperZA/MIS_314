0c34dca3c513a09f44a18f04e089bb60bebf8a2a
CooperZA <zach@raincitysolutions.com>
2020-01-12 19:17:58 -0800

    initial commit

---
+++ A01/hello_world.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello_World</title>
+        <link rel="stylesheet" href="hello_world.css"/>
+    </head>
+    <body>
+        <div id="wrapper">
+            <div class="container">
+                <?php
+                for($i; $i < 10; $i++){
+                echo "<h1> Hello World! </h1>";
+                }
+                ?>
+            </div>
+        </div>
+    </body>
+</html>
