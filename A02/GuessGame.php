f59572fd3d993ac798d55a1bc512dc70cc8faa8b
CooperZA <zach@raincitysolutions.com>
2020-01-16 20:22:55 -0800

    assignment 2: variables

---
+++ A02/GuessGame.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sample web form</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <body>
+        <div class="pageContainer centerText">
+
+            <h2>Welcome to the Number Guess Game</h2>
+            <hr />
+
+            <p>Enter a number in the text box</p>
+
+            <form>
+                <p>Please enter a number:</p>
+                <input type="text" name="fname" autofocus>
+                <input type="submit" value="Go">
+            </form>
+
+            <?php
+            //Retrieve name from querystring. Check that parameter
+            //is in querystring or may get "Undefined index" error
+            if (isset($_GET['fname'])) {
+                $fname = $_GET['fname'];
+                echo "<h1> Hello $fname";
+            }
+            ?>
+        </div>
+
+    </body>
+
+</html>
