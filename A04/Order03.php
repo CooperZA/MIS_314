74fa7dbf028d98e7876d5189daa7f2748a4c6d3a
Zach Cooper <zach@raincitysolutions.com>
2020-02-04 13:27:44 -0800

    assignment 4 - 1/3 done

---
+++ A04/Order03.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+    <title>Order Confirmation</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="pageContainer centerText">
+        <p></p>
+        <h2>Order Confirmation</h2>
+
+        <h3> Congratulations zach you
+            have ordered a blue Mustang!</h3>
+        <img style='margin: 30px 0;' src='/sandvig/mis314/assignments/a04/images/Mustangblue.jpg'>
+        <br>
+        <a href="Order01.php">Place another order</a>
+
+    </div>
+</body>
+
+</html>
