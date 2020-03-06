56fa8dd679033cb9df4d867bed58cd32a5189a57
Zach Cooper <zach@raincitysolutions.com>
2020-03-06 11:58:13 -0800

    add jquery lib

--- A07/include/footer.php
+++ A07/include/footer.php
@@ -27,6 +27,7 @@
 </body>
 <!-- jQuery -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="node_modules/jquery/dist/jquery.js"></script>
 <!-- bootstrap -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 <!-- custom js -->
